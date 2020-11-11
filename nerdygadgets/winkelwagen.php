2e7fc23f709029c13a6cc6716718118dcb4679e3
Reqult <35534797+EvilSlime@users.noreply.github.com>
2020-11-11 15:08:43 +0100

    Reclame naar actie

--- nerdygadgets/winkelwagen.php
+++ nerdygadgets/winkelwagen.php
@@ -16,7 +16,7 @@ include __DIR__ . "/header.php";
             <p>couponcode</p>
             <input type="number" value="1" name="coupons" class="couponNumber" readonly>
         </div>
-            <input type="checkbox" name="reclameMail" class="reclameMail"><label for="reclameMail">Ik zou graag reclame via de e-mail willen ontvangen</label>
+            <input type="checkbox" name="reclameMail" class="reclameMail"><label for="reclameMail">Ik zou graag acties via de e-mail willen ontvangen</label>
             <input type="submit" name="submit" value="Betaal pagina">
     </form>
 </div>
