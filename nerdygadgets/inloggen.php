6c68170cdc72b1f2ec3f2d024624f0c69fc260ce
Reqult <35534797+EvilSlime@users.noreply.github.com>
2020-11-12 11:49:09 +0100

    Update inloggen.php

--- nerdygadgets/inloggen.php
+++ nerdygadgets/inloggen.php
@@ -35,21 +35,19 @@ include __DIR__ . "/header.php";
     </style>
 </head>
 <body>
-    <h2>Enter Username and Password</h2>
+    <h2>Log in met je e-mailadress</h2>
       <div class = "container form-signin">
          <?php
             if (isset($_POST['login']) && !empty($_POST['username'])
                && !empty($_POST['password'])) {
-
-               if ($_POST['username'] == 'Test' &&
-                  $_POST['password'] == 'Test') {
+               if ($_POST['username'] == 'Test' && $_POST['password'] == 'Test') {
                   $_SESSION['valid'] = true;
                   $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
+                  $_SESSION['username'] = 'Test';
 
-                  echo 'You have entered a valid username and password';
+                  echo 'U heeft een geldende naam en wachtwoord ingevuld!';
                }else {
-                  echo 'Wrong username or password';
+                  echo 'Onjuiste naam of wachtwoord.';
                }
             }
          ?>
@@ -58,10 +56,11 @@ include __DIR__ . "/header.php";
          <form class = "form-signin" role = "form"
             action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
             <input type = "text" class = "form-control"
-               name = "username" placeholder = "username = Test" required autofocus></br>
+               name = "username" placeholder = "Naam = Test" required autofocus></br>
             <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = Test" required>
+               name = "password" placeholder = "Wachtwoord = Test" required>
             <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "login">Login</button>
+             Nog geen account? Registreer een account door hierop te klikken!
          </form>
       </div>
    </body>
