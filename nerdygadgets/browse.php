44c8ec9380609f6a69c98e22371804c26ed2ed48
yes-102 <74291142+yes-102@users.noreply.github.com>
2020-11-12 11:59:33 +0100

    Update browse.php

--- nerdygadgets/browse.php
+++ nerdygadgets/browse.php
@@ -162,11 +162,11 @@ if (isset($amount)) {
 <div id="FilterFrame"><h2 class="FilterText"><i class="fas fa-filter"></i> Filteren </h2>
     <form>
         <div id="FilterOptions">
-            <h4 class="FilterTopMargin"><i class="fas fa-search"></i> Zoeken</h4>
+            <h5 class="FilterTopMargin"><i class="fas fa-search"></i> Zoeken</h5>
             <input type="text" name="search_string" id="search_string"
                    value="<?php print (isset($_GET['search_string'])) ? $_GET['search_string'] : ""; ?>"
                    class="form-submit">
-            <h4 class="FilterTopMargin"><i class="fas fa-list-ol"></i> Aantal producten op pagina</h4>
+            <h5 class="FilterTopMargin"><i class="fas fa-list-ol"></i> Aantal producten op pagina</h5>
 
             <input type="hidden" name="category_id" id="category_id"
                    value="<?php print (isset($_GET['category_id'])) ? $_GET['category_id'] : ""; ?>">
@@ -183,8 +183,12 @@ if (isset($amount)) {
                     print "selected";
                 } ?>>75
                 </option>
+                <option value="100" <?php if ($_SESSION['products_on_page'] == 100) {
+                    print "selected";
+                } ?>>100
+                </option>
             </select>
-            <h4 class="FilterTopMargin"><i class="fas fa-sort"></i> Sorteren</h4>
+            <h5 class="FilterTopMargin"><i class="fas fa-sort"></i> Sorteren</h5>
             <select name="sort" id="sort" onchange="this.form.submit()">>
                 <option value="price_low_high" <?php if ($_SESSION['sort'] == "price_low_high") {
                     print "selected";
