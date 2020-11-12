fa5ea1b5350f938c0ff11b313290eb4cbb399250
GeoWitmond <74290903+GeoWitmond@users.noreply.github.com>
2020-11-12 10:53:38 +0100

    login

--- nerdygadgets/inloggen.php
+++ nerdygadgets/inloggen.php
@@ -1,3 +1,7 @@
 <?php
+$Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
+mysqli_set_charset($Connection, 'latin1');
+include __DIR__ . "/header.php";
+
 print ("Hello World");
 ?>
