48e45c6afb629d08b38c4231a5b861e47db65f8a
leonvk <leonvk2000@gmail.com>
2020-11-18 10:23:16 +0100

    bestelpagina styling

--- nerdygadgets/bestellen.php
+++ nerdygadgets/bestellen.php
@@ -11,38 +11,25 @@ include __DIR__ . "/header.php";
       </div>
       <div class = "container">
           <h2>Kassa</h2>
-         <form class = "form-signin" role = "form"
-            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
-             <div class = "Naam">
-             Naam<br>
-             <input type = "text" class = "form-control"
-               name = "shipment_address_first_name" placeholder = "Voornaam" required autofocus>
-             <input type = "text" class = "form-control"
-                    name = "shipment_address_name_addition" placeholder = "Tussenv." required>
-             <input type = "text" class = "form-control"
-                    name = "shipment_address_last_name" placeholder = "Achternaam" required><br>
+         <form class = "form-signin" role = "form" action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method = "post">
+             <div class = "bestellenKolom1">
+                Naam:<br>
+                <input type = "text" class = "form-control" name = "shipment_address_first_name" placeholder = "Voornaam" required autofocus>
+                <input type = "text" class = "form-control" name = "shipment_address_name_addition" placeholder = "Tussenv." required>
+                <input type = "text" class = "form-control" name = "shipment_address_last_name" placeholder = "Achternaam" required><br>
              </div>
-             <div class = "Postcode">
-             Postcode<br>
-                 <input type = "text" class = "form-control"
-                        name = "shipment_address_post_code" placeholder = "Postcode" required>
+             <div class = "bestellenKolom2">
+                 Postcode:<br>
+                 <input type = "text" class = "form-control" name = "shipment_address_post_code" placeholder = "Postcode" required>
+                 Huisnummer:<br>
+                 <input type = "text" class = "form-control" name = "shipment_address_house_number" placeholder = "Nr." required>
              </div>
-             <div class = "Huisnummer">
-             Huisnummer<br>
-                 <input type = "text" class = "form-control"
-                    name = "shipment_address_house_number" placeholder = "Nr." required>
-             </div>
-             <div class = "email">
-                 E-maildres<br>
-                 <input type = "email" class = "form-control"
-                        name = "email" placeholder = "example@nerdygadgets.com" required>
-             </div>
-             <div class = "Wachtwoord">
-                 Wachtwoord<br>
-                 <input type = "password" class = "form-control"
-                        name = "wachtwoord" placeholder = "Wachtwoord" required>
-                 <input type = "password" class = "form-control"
-                        name = "wachtwoord" placeholder = "Wachtwoord bevestigen" required><br>
+             <div class = "bestellenKolom3">
+                 E-maildres:<br>
+                 <input type = "email" class = "form-control" name = "email" placeholder = "example@nerdygadgets.com" required>
+                 Wachtwoord:<br>
+                 <input type = "password" class = "form-control" name = "wachtwoord" placeholder = "Wachtwoord" required>
+                 <input type = "password" class = "form-control" name = "wachtwoord" placeholder = "Wachtwoord bevestigen" required><br>
              </div>
              <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "login">Doorgaan</button>
          </form>
