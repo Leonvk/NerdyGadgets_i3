8d11038e39b3a3f38d17ec82d28d1a10eb2c7a75
yes-102 <74291142+yes-102@users.noreply.github.com>
2020-11-18 13:39:57 +0100

    footer fix :D

--- nerdygadgets/footer.php
+++ nerdygadgets/footer.php
@@ -1,6 +1,82 @@
++<!-- Footer -->
+<footer id="footer">
+
+<!-- Footer Links -->
+<div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+
+            <!-- Content -->
+            <h5 class="text-uppercase">Footer Content</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend justo ligula, sed maximus neque tristique vel..</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+            <!-- Links -->
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled">
+                <li>
+                    <a href="https://www.youtube.com/watch?v=oHg5SJYRHA0&ab_channel=cotter548">Link 1</a>
+                </li>
+                <li>
+                    <a href="index.php">Link 2</a>
+                </li>
+                <li>
+                    <a href="index.php">Link 3</a>
+                </li>
+                <li>
+                    <a href="index.php">Link 4</a>
+                </li>
+            </ul>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+            <!-- Links -->
+            <h5 class="text-uppercase">Contact</h5>
+
+            <ul class="list-unstyled">
+                <li>
+                    <p>Nerdygadgets@nerdygadgets.nl</p>
+                </li>
+                <li>
+                    <p>030 123 12 34</p>
+                </li>
+                <li>
+                    <a href="#!">Link 3</a>
+                </li>
+                <li>
+                    <a href="#!">Link 4</a>
+                </li>
+            </ul>
+
+        </div>
+        <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
 </div>
+<!-- Footer Links -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> NerdyGadgets.com</a>
 </div>
-</div>
-</div>
-</body>
-</html>
+<!-- Copyright -->
+<!-- Footer -->
+</footer>
