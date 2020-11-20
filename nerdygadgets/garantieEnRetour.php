20ae3bedae7c4c4dcae714a44f8f498cc6792f2f
GeoWitmond <74290903+GeoWitmond@users.noreply.github.com>
2020-11-20 12:24:00 +0100

    Garantie en retour
    garantie en retour text toegevoegd

--- nerdygadgets/garantieEnRetour.php
+++ nerdygadgets/garantieEnRetour.php
@@ -1,3 +1,46 @@
 <?php
-print("U heeft recht op garatie en kan producten retour sturen ;)");
+include __DIR__ . "/header.php";
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+    <h1>Retouneren</h1>
+    <h5>Retourtermijn</h5>
+        <p> Voor artikelen die op een afstand worden verkocht, geldt een wettelijk herroepingsrecht van 14 dagen. Verder heeft u op bepaalde producten ook nog wettelijke garantie zitten.<br>
+            In deze periode kunt u het product ook nog retouneren als het niet meer werkende is.</p>
+    <h5>Wat je mag retrouneren?</h5>
+        <p>Binnen het retourtermijn mag je bestelde artikelen redelijkerwijs uitproberen om te weten of het naar behoren werkt en of je er niet blij mee bent. <br>
+        Net zoals het passen van kleding en schoenen in winkels, mag je een artikel dat via de website is bestelt, testen om te kijken of het bevalt en voldoet aan je eisen.<br>
+        Verder mag je het artikel kosteloos retouneren. Je mag het artikel nog niet volledig in gebruik nemen als je nog niet weet of je het houdt.</p>
+    <h5>Wat je niet kan retouneren</h5>
+    <p>De volgende type artikelen kunnen niet worden geretouneerd:</p>
+        <ul>
+            <li>Artikelen die om redenen van gezondheidsbescherming of hygiëne niet retour mogen (waarvan de verzegeling is verbroken).</li>
+            <li>Artikelen met gebruikerssporen zoals kleding en schoenen (artikel mag niet gedragen zijn, het label moet er nog aan zitten).</li>
+            <li>Artikelen die in volle mate zijn gebruikt.</li>
+            <li>Voedingsmiddelen</li>
+        </ul>
+<br><br>
+    <h1>Garantie</h1>
+    <h5>Fabrieksgarantie</h5>
+        <p>De fabrikant van je bestelde artikel kan jou fabrieksgarantie geven. Dit wil zeggen dat de fabrikant tijdens die garantieduur je artikel repareert of vervangt als die defect raakt, <br>
+            tenzij het defect is ontstaan door jouw toedoen. Je kan hiervoor contact opnemen met de fabrikant. <br><br>
+
+            De fabrieksgarantie verschilt per fabrikant. In de verpakking van je artikel zit een boekje met meer details. <br>
+            Let op: op sommige artikelen zit minder of geen fabrieksgarantie. Denk hierbij bijvoorbeeld aan "verbruiksartikelen" zoals shampoo of luiers.</p>
+    <h5>Wettelijke garantie</h5>
+        <p>Als er geen fabrieksgarantie geldt of als je toch liever naar de verkoper stapt, bestaat daarnaast nog de wettelijke garantie. <br>
+            Deze garantie houdt in dat je gekochte artikel moet voldoen aan wat beloofd is en wat je redelijkerwijs mocht verwachten. <br>
+            Als je bijvoorbeeld een televisie hebt gekregen waarvan de afstandsbediening al na een paar weken kapot gaat, mag je de verkoper hierover contacteren. <br>
+            Wacht hier niet te lang mee nadat je dit gebrek hebt ontdekt. Als de periode van de fabrieksgarantie is afgelopen, wil dat dus niet zeggen dat er helemaal geen garantie meer is. <br><br>
+
+            Mocht er een defect ontstaan buiten jouw schuld om, dankun je recht hebben op een reparatievergoeding of vervanging van het product. Hieronder staat wat je dan kan doen.</p>
+    <h5>Een defect artikel</h5>
+        <p>Om zo snel mogelijk weer een goedwerkend artikel in huis te hebben kun je het beste retourneren en een nieuwe bestellen. <br>
+            Meld het artikel als retour aan in je account of check de retourvoorwaarden of jouw artikel retour gestuurd mag worden.</p>
+    </body>
+</html>
+
+<?php
+include __DIR__ ."/footer.php";
 ?>
