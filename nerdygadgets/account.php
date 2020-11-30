d0e392c4ac109e88d7dd9a594759a9d41ade7b0e
Jesse <jessebrand2002@gmail.com>
2020-11-30 15:33:22 +0100

    Revert "Revert "Revert "account.php minimaal gestyled"""
    This reverts commit 446de3882e5cfe0daea4660d62ce99569e38594b.

--- nerdygadgets/account.php
+++ nerdygadgets/account.php
@@ -65,20 +65,17 @@ $number = $result[0]['number'];
 
 if(isset($error)) {echo($error);}
 ?>
-<div class="accInfo">
 <h2>Overzicht van het account <?php echo($username); ?></h2>
-    <div class="accInfo2">
-        <button><a href="orders.php">Mijn bestellingen</a></button>
-        <div id="accountOverview">
-            <?php
-            echo("Gebruikersnaam: $username<br>Voornaam: $firstName<br>Tussenvoegsel: $middleName<br>Achternaam: $lastName<br>e-mail: $email<br>Gebruiker sinds: $userSince<br>Telefoonnummer: $phone<br>Postcode: $postalCode<br>Huisnummer: $number");
-            ?>
-        </div>
-    </div>
+<button><a href="orders.php">Mijn bestellingen</a></button>
+<div id="accountOverview">
+<?php
+
+    echo("Gebruikersnaam: $username<br>Voornaam: $firstName<br>Tussenvoegsel: $middleName<br>Achternaam: $lastName<br>e-mail: $email<br>Gebruiker sinds: $userSince<br>Telefoonnummer: $phone<br>Postcode: $postalCode<br>Huisnummer: $number");
+?>
+</div>
 <form method="post">
     <input type="submit" value="Uitloggen" name="logOut" id="AccountUitlogKnop">
 </form>
-
 
 <?php
 if(!isset($_POST['delete'])) {
@@ -95,7 +92,6 @@ if(!isset($_POST['delete'])) {
             <input type=\"submit\" value=\"Ik bevestig hiermee dat ik al mijn accountgegevens kwijt raak en ik geen toegang meer heb tot mijn account.\" name=\"deleteConfirm\" style=\"background-color: red\">
             <input type=\"submit\" value=\"Annuleren\" name=\"deleteCancel\" style=\"background-color: green\">
         </form>
-    </div>
     </div>
     "); 
 }
