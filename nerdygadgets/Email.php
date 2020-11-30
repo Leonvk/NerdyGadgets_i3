3f4b80ed194d378769eb701eaa64857d4e33773f
GeoWitmond <74290903+GeoWitmond@users.noreply.github.com>
2020-11-30 16:59:27 +0100

    van email veld en een verplicht type

--- nerdygadgets/Email.php
+++ nerdygadgets/Email.php
@@ -6,15 +6,18 @@ include __DIR__ . "/header.php";
     <form action="EmailContactConfirmatie.php" method="post"?>
 
         <div id="EmailForSubject">
-            <label for="EmailAdres">For:</label>
-            <input id="EmailAdres" name="EmailBox" value="Nerdy.gadgets@nerdygadgets.nl" disabled> <br><br>
+            <label for="EmailAdres">Voor:</label>
+            <input id="EmailAdres" name="EmailBox" type="email" value="Nerdy.gadgets@nerdygadgets.nl" disabled> <br><br>
 
-            <label for="EmailOnderwerp">Subject:</label>
-            <input id="EmailOnderwerp" name="EmailOnderwerp"><br><br>
+            <label for="EmailVan">Van?</label>
+            <input id="EmailVan" name="EmailVan" type="email" required>
+
+            <label for="EmailOnderwerp">Onderwerp:</label>
+            <input id="EmailOnderwerp" name="EmailOnderwerp" required><br><br>
         </div>
 
         <label for="EmailText"></label>
-        <textarea id="EmailText" name="EmailText" rows="15" cols="360" style="resize: none"></textarea>
+        <textarea id="EmailText" name="EmailText" aria-required="true" rows="15" cols="360" style="resize: none"></textarea>
         <input type="submit" value="Send" id="SendKnopEmail">
     </form>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
--- nerdygadgets/EmailContactConfirmatie.php
+++ nerdygadgets/EmailContactConfirmatie.php
@@ -2,7 +2,7 @@
 include __DIR__ . "/header.php";
 ?>
 
-<h1>Wij hebben uw bericht ontvangen</h1>
+<h3>Wij hebben uw bericht ontvangen</h3>
 
 <h5>Wij gaan zo snel mogelijk aan de slag om uw vragen te beantwoorden <br><br>
 U kunt binnen 5 werkdagen een reactie van ons verwachten. <br>
@@ -14,7 +14,7 @@ Als u binnen 5 werkdagen nog niet van ons te horen heeft gekregen
     <form action="Email.php" method="get">
         <div id="EmailOnderwerpConfirmatie">
         <?php
-        print ($_POST["EmailOnderwerp"]);
+        echo ($_POST["EmailOnderwerp"]);
         ?>
         </div>
         <br><br>
