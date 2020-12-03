90e2b5db00a45aed19de923ec6c99a70da480e85
GeoWitmond <74290903+GeoWitmond@users.noreply.github.com>
2020-12-03 09:48:08 +0100

    Update EmailContactConfirmatie.php

--- nerdygadgets/EmailContactConfirmatie.php
+++ nerdygadgets/EmailContactConfirmatie.php
@@ -2,8 +2,8 @@
 include __DIR__ . "/header.php";
 ?>
 <div id="EmailRetourBericht">
-    <h4>Wij hebben een bericht ontvangen van <?php echo ($_POST["EmailVan"])?>.<br>
-        Met als onderwerp <?php echo ($_POST["EmailOnderwerp"]) ?><br><br>
+    <h4>Wij hebben een bericht ontvangen van: <?php echo ($_POST["EmailVan"])?>.<br>
+        Met als onderwerp; <?php echo ($_POST["EmailOnderwerp"]) ?><br><br>
     Als u binnen 5 werkdagen nog geen antwoord van ons heeft ontvangen neem dan telefonisch contact met ons op.<br>
     Wij staan elke werkdag van 9 tot 18 klaar om u verder te helpen</h4>
 
